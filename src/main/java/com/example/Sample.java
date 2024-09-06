48c3f2a6b9ca355eedc0f00a3af0888ef8da5118
Kazuhiro Oka <k.oka@kazutech.jp>
2024-09-07 01:33:10 +0900

    update: Sample

--- src/main/java/com/example/Sample.java
+++ src/main/java/com/example/Sample.java
@@ -2,7 +2,9 @@ package com.example;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.guild.MemberJoinEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
 import discord4j.core.object.entity.channel.MessageChannel;
 
@@ -12,11 +14,27 @@ public class Sample {
 		DiscordClient client = DiscordClient.create(token);
 		GatewayDiscordClient gateway = client.login().block();
 		
+		/* ------- ログイン完了 ------- **/
+		
 		gateway.on(MessageCreateEvent.class).subscribe(event -> {
+			/* メッセージ投稿イベント **/
+			// メッセージを取得
 			Message message = event.getMessage();
 			if ("!ping".equals(message.getContent())) {
 				MessageChannel channel = message.getChannel().block();
 				channel.createMessage("Pong!").block();
+			}
+		});
+		
+		gateway.on(MemberJoinEvent.class).subscribe(event -> {
+			/* メンバー参加イベント **/
+			// メンバーを取得
+			Member member = event.getMember();
+			MessageChannel channel = member.getPrivateChannel().block();
+			if(channel == null) {
+				System.out.println("DMチャンネルが取得できませんでした。");
+			}else{
+				channel.createMessage("ようこそ！").block();
 			}
 		});
 		
