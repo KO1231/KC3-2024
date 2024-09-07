4f189b501d41948ba030d508b77bafed03a10ebc
Kazuhiro Oka <k.oka@kazutech.jp>
2024-09-07 12:41:51 +0900

    add: guessing game sample

--- src/main/java/com/example/Sample.java
+++ src/main/java/com/example/Sample.java
@@ -37,6 +37,37 @@ public class Sample {
 			}
 		});
 		
+		gateway.on(MessageCreateEvent.class).subscribe(event -> {
+			/* メッセージ投稿イベント **/
+			// メッセージを取得
+			Message message = event.getMessage();
+			MessageChannel channel = message.getChannel().block();
+			String messageContent = message.getContent();
+			
+			if(channel == null){
+				System.out.println("メッセージの送信元チャンネルが取得できませんでした。");
+			}else{
+				if(messageContent.startsWith("!guess")){
+					String[] content = messageContent.split(" ");
+					if(content.length == 2){
+						try{
+							int answer = Integer.parseInt(content[1]);
+							int guess = (int)(Math.random() * 10);
+							if(answer == guess){
+								channel.createMessage("正解！").block();
+							}else{
+								channel.createMessage("不正解！ 正解は" + guess + "です。").block();
+							}
+						}catch(NumberFormatException e){
+							channel.createMessage("数値を入力してください。").block();
+						}
+					}else{
+						channel.createMessage("引数が不正です。").block();
+					}
+				}
+			}
+		});
+		
 		gateway.on(MemberJoinEvent.class).subscribe(event -> {
 			/* メンバー参加イベント **/
 			// メンバーを取得
